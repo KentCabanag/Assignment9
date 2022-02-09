0386bbea2d8893951fccf8a210202868707e02f9
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2022-02-10 01:53:14 +0800

    commit

--- Program1(PLD9).py
+++ Program1(PLD9).py
@@ -16,8 +16,8 @@
 from fpdf import FPDF
 import json
 
-pdf = FPDF('P', 'cm', 'A4') 
-pdf.add_page()
+file = FPDF('P', 'cm', 'A4') 
+file.add_page()
 
 with open('personalresume.json') as datafile:
 	info = json.load(datafile)
@@ -25,13 +25,13 @@ with open('personalresume.json') as datafile:
 # Personal details
 First_name = info ["personal info"]["First Name"]
 Middle_Initial = info ["personal info"]["Middle Initial"]
-Last_name = info  ["personal info"]["Last Name"]
-Place_of_Birth = info  ["personal info"]["Place of Birth"]
-Gender = info  ["personal info"]["Gender"]
-Date_of_Birth = info  ["personal info"]["Date of Birth"]
-Marital_Status = info  ["personal info"]["Marital Status"]
-Religion = info  ["personal info"]["Religion"]
-Nationality = info  ["personal info"]["Nationality"]
+Last_name = info ["personal info"]["Last Name"]
+Place_of_Birth = info ["personal info"]["Place of Birth"]
+Gender = info ["personal info"]["Gender"]
+Date_of_Birth = info ["personal info"]["Date of Birth"]
+Marital_Status = info ["personal info"]["Marital Status"]
+Religion = info ["personal info"]["Religion"]
+Nationality = info ["personal info"]["Nationality"]
 
 # Objective
 Objective = info ["Objective"]["Career Objective"]
@@ -39,6 +39,7 @@ Objective = info ["Objective"]["Career Objective"]
 # Contact Information
 Contact = info ["Contact Information"]["Contact"]
 Email = info ["Contact Information"] ["Email"]
+Address = info ["Contact Information"] ["Address"] 
 
 # Educational Background
 College = info ["Educational Background"]["2014-2018"]
@@ -50,3 +51,73 @@ skill_1 = info ["Skills"]["skill 1"]
 skill_2 = info ["Skills"]["skill 2"] 
 skill_3 = info ["Skills"]["skill 3"] 
 skill_4 = info ["Skills"]["skill 4"]
+
+file.set_font("helvetica", "B", 24)
+file.set_text_color(40,51,49)
+file.set_fill_color(215,215,215)
+file.cell(19, 3, f"{info['personal info']['First Name']}{info['personal info']['Middle Initial']}{info['personal info']['Last Name']}",ln=1, align="C", fill=1)
+
+file.set_text_color(255,165,0)
+file.set_fill_color(0,0,200)
+file.set_font("Arial", "B", 15)
+file.cell(19,1," Personal Details", align="L", ln=2, fill=1)
+file.image('picture.jpg',1, 1, 3.8)
+
+file.set_font("Arial", "")
+file.set_font_size(12)
+file.set_text_color(40,40,40)
+file.cell(0,0.9," Place of Birth:  " + (info["personal info"]["Place of Birth"]),align="L", ln=1, fill=0)
+file.cell(0,0.9," Gender:  " + (info["personal info"]["Gender"]),align="L", ln=1, fill=0)
+file.cell(0,0.9," Date of Birth:  " + (info["personal info"]["Date of Birth"]),align="L", ln=1, fill=0)
+file.cell(0,0.9," Marital Status:  " + (info ["personal info"]["Marital Status"]),align="L", ln=1, fill=0)
+file.cell(0,0.9," Religion:  " + (info ["personal info"]["Religion"]),align="L", ln=1, fill=0)
+file.cell(0,0.9," Nationality:  " + (info ["personal info"]["Nationality"]),align="L", ln=1, fill=0)
+
+file.set_text_color(255,165,0)
+file.set_fill_color(0,0,200)
+file.set_font("Arial", "B", 15)
+file.cell(19,1," Objective", align="L", ln=2, fill=1)
+
+file.set_font("Arial", "")
+file.set_font_size(12)
+file.set_text_color(40,40,40)
+file.multi_cell(0,0.9, info["Objective"]["Career Objective"])
+
+file.set_text_color(255,165,0)
+file.set_fill_color(0,0,200)
+file.set_font("Arial", "B", 15)
+file.cell(19,1," Contact Information", align="L", ln=2, fill=1)
+
+file.set_font("Arial", "")
+file.set_font_size(12)
+file.set_text_color(40,40,40)
+file.cell(0,0.9," Contact Number:  " + (info["Contact Information"]["Contact"]),align="L", ln=1, fill=0)
+file.cell(0,0.9," Email:  " + (info["Contact Information"] ["Email"]),align="L", ln=1, fill=0)
+file.cell(0,0.9," Address:  " + (info["Contact Information"] ["Address"]),align="L", ln=1, fill=0)
+
+file.set_text_color(255,165,0)
+file.set_fill_color(0,0,200)
+file.set_font("Arial", "B", 15)
+file.cell(19,1," Educational Background", align="L", ln=2, fill=1)
+
+file.set_font("Arial", "")
+file.set_font_size(12)
+file.set_text_color(40,40,40)
+file.cell(0,0.9," 2014-2018:  " + (info["Educational Background"]["2014-2018"]),align="L", ln=1, fill=0)
+file.cell(0,0.9," 2010-2014:  " + (info["Educational Background"]["2010-2014"]),align="L", ln=1, fill=0)
+file.cell(0,0.9," 2006-2010:  " + (info["Educational Background"]["2006-2010"]),align="L", ln=1, fill=0)
+
+file.set_text_color(255,165,0)
+file.set_fill_color(0,0,200)
+file.set_font("Arial", "B", 15)
+file.cell(19,1," Skills", align="L", ln=2, fill=1)
+
+file.set_font("Arial", "")
+file.set_font_size(12)
+file.set_text_color(40,40,40)
+file.cell(0,0.9, (info["Skills"]["skill 1"]),align="L", ln=1, fill=0)
+file.cell(0,0.9, (info["Skills"]["skill 2"]),align="L", ln=1, fill=0)
+file.cell(0,0.9, (info["Skills"]["skill 3"]),align="L", ln=1, fill=0)
+file.cell(0,0.9, (info["Skills"]["skill 4"]),align="L", ln=1, fill=0)
+
+file.output("CABANAG_KENTANGELO.pdf")
